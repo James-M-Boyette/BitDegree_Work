6ed4234f9aae695e2297dad47ecdd3089d2f276a
sou7hernsaint <James.M.84.Boyette@gmail.com>
2022-02-25 17:09:27 -0500

    Add JavaScript Sprint 8 Assignment Files via upload

---
+++ 4.12.Assignment/javascript-playground/app.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    // console.log('This is your first program executing!');
+    console.log(`Hi Neighbor! 
+
+
+    You know what we always say: "We have better prices than our competition - *every* time!"
+
+
+    And make sure to check our “latest discounts” page!`);
+
+}
