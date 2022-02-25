388b9099e6f167e86cdfb324a4308dd11e588b57
sou7hernsaint <James.M.84.Boyette@gmail.com>
2022-02-25 16:10:41 -0500

    Add Sprint 8 Assignment Files

---
+++ Sprint 8/Assignments/Assignment_6_5/test.rb
@@ -0,0 +1,24 @@
+# EXAMPLE 1:
+# x = [1, 2, 3, 4, 5]
+
+# for i in x.reverse
+#   puts i
+# end
+
+# puts "Done!"
+
+# EXAMPLE 2:
+# x = gets.chomp.to_i
+
+# puts "space!"
+
+# for i in 1..x
+#   puts x - i
+# end
+
+# puts "Done!"
+
+# EXAMPLE 3:
+for number_of_elements in [5, 10, 15, 20, 25]
+  pp number_of_elements
+end
